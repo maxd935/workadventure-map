f7a96ea4389b55217e3b03ab666011cec07671fd
Maxime Dev <maximedaude.dev@gmail.com>
2022-06-11 09:29:53 +0200

    change map

---
+++ assets/tileset/Map Collection.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Map" tilewidth="1163" tileheight="1097" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/bet_zone-removebg-preview.png"/>
+ </tile>
+ <tile id="1">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/bet_zone_2-removebg-preview.png"/>
+ </tile>
+ <tile id="2">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/bet_zone-removebg-preview.png"/>
+ </tile>
+ <tile id="3">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/ecran_geant-removebg-preview (1).png"/>
+ </tile>
+ <tile id="4">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/foodtruck-removebg-preview.png"/>
+ </tile>
+ <tile id="5">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/herbe-removebg-preview.png"/>
+ </tile>
+ <tile id="6">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/logo_partenaire-removebg-preview.png"/>
+ </tile>
+ <tile id="7">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/map_base-removebg-preview.png"/>
+ </tile>
+ <tile id="8">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/map_bet_adeventure_copie-removebg-preview.png"/>
+ </tile>
+ <tile id="9">
+  <image width="1163" height="1097" source="../../../../../Downloads/work adventure map/map.png"/>
+ </tile>
+ <tile id="10">
+  <image width="514" height="485" source="../../../../../Downloads/work adventure map/shop-removebg-preview.png"/>
+ </tile>
+</tileset>
